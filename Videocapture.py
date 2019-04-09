56b229a34aa0e9a05279f3175e30786bd18571f1
GusseJensen <38212446+GusseJensen@users.noreply.github.com>
2019-04-09 15:03:40 +0200

    Update Videocapture.py

--- Videocapture.py
+++ Videocapture.py
@@ -31,17 +31,17 @@ def print_frame(frame_data, thisType):
     #QVGA is what my camera defaulted to, so: 1 x 240 x 320
     #also order was weird (1, 240, 320) not (320, 240, 1)
 
-    if whatisit == (320*240*1):#QVGA
+    if whatisit == (640*480*1):#QVGA
         #shape it accordingly, that is, 1048576=1024*1024
-        img.shape = (1, 240, 320)#small chance these may be reversed in certain apis...This order? Really?
+        img.shape = (1, 480, 640)#small chance these may be reversed in certain apis...This order? Really?
         #filling rgb channels with duplicates so matplotlib can draw it (expects rgb)
         img = np.concatenate((img, img, img), axis=0)
         #because the order is so weird, rearrange it (third dimension must be 3 or 4)
         img = np.swapaxes(img, 0, 2)
         img = np.swapaxes(img, 0, 1)
-    elif whatisit == (320*240*3):
+    elif whatisit == (480*640*3):
         #color is miraculously in this order
-        img.shape = (240, 320, 3)
+        img.shape = (480, 640, 3)
 
     else:
         print(f"Frames are of size {img.size}")
@@ -58,14 +58,14 @@ dev = openni2.Device.open_any()
 
 print(dev.get_sensor_info(openni2.SENSOR_DEPTH) )
 depth_stream = dev.create_depth_stream()
-depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX = 320, resolutionY = 240, fps = 30))
+depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX = 640, resolutionY = 480, fps = 30))
 depth_stream.start()
 frame = depth_stream.read_frame()
 frame_data = frame.get_buffer_as_uint16()
 print_frame(frame_data, np.uint16)
 depth_stream.stop()
 
-depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_100_UM, resolutionX = 320, resolutionY = 240, fps = 30))
+depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_100_UM, resolutionX = 640, resolutionY = 480, fps = 30))
 depth_stream.start()
 frame = depth_stream.read_frame()
 frame_data = frame.get_buffer_as_uint16()
@@ -74,7 +74,7 @@ depth_stream.stop()
 
 print("Testing Color ")
 color_stream = dev.create_color_stream()
-color_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888, resolutionX = 320, resolutionY = 240, fps = 30))
+color_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888, resolutionX = 640, resolutionY = 480, fps = 30))
 color_stream.start()
 frame = color_stream.read_frame()
 frame_data1 = frame.get_buffer_as_uint8()
